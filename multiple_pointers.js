5434ae2f15e1d8429e5f35e27aef861fb6043310
Leaundrae Mckinney <pytechdrae@gmail.com>
2022-09-20 08:30:34 -0400

    Quick fix

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -1,13 +1,15 @@
 
 /* Cell ID: 0km0u44e*/
-/*/*# Multiple Pointers
+/*# Multiple Pointers
 
-### Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition*/
+### Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
 */
 
 /* Cell ID: crkyg06o*/
 /**
- * Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+ * Write a function called sumZero which accepts a sorted array of integers. 
+ * The function should find the first pair where the sum is 0. 
+ * Return an array that includes both values that sum to zero or undefined if a pair does not exist.
  */
 
  const sumZero = (arr) => {
