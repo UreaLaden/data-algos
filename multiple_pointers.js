4934104b405cf9bb7bdedc8f42197e353fad79b7
Leaundrae Mckinney <pytechdrae@gmail.com>
2022-09-20 09:31:43 -0400

    Count Unique Values

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -32,8 +32,61 @@
   }
 }
 
-console.log(sumZero([-3,-2,-1,0,1,2,3])) // [-3,3]
-console.log(sumZero([-2,0,1,3])) // undefined
-console.log(sumZero([1,2,3])) //undefined
+// console.log(sumZero([-3,-2,-1,0,1,2,3])) // [-3,3]
+// console.log(sumZero([-2,0,1,3])) // undefined
+// console.log(sumZero([1,2,3])) //undefined
 
+
+
+
+/* Cell ID: m47169qn*/
+/*
+* Implement a function called countUniqueValues, which accepts a sorted 
+* array, and counts the unique values in the array. There can be negative
+* numbers in the array, but it will always be sorted
+*/
+/*# Understanding the Problem
+1.  Can I restate the problem in my own words?
+--> Count every unique value in a sorted array
+2.  What are the inputs that go into the problem?
+--> sorted array, of positive and negative numbers
+3.  What are the outputs that should come from the solution to the problem?
+--> integer
+4.  Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+5.  How should I label the important pieces of data that are a part of the problem?
+ 
+*/
+
+const countUniqueValues = (arr) => {
+  //determine length of the array
+  let count = 0
+  let length = arr.length
+  if (length === 0) return count
+  //set left to beginning and right to end
+  let left = 0
+  let right = length - 1  
+  // Iterate while the left is smaller than the length 
+  while (left < length){
+    let leftNum = arr[left];
+    let rightNum = arr[right];
+  //if the value at the right > value at left, decrement right
+    if (rightNum > leftNum){
+      right--;
+    }
+  //if the value at left === value at right
+  // increment count and set left to right +1 and right to end
+    if (leftNum === rightNum){
+      count++;
+      left = right + 1
+      right = length - 1
+    }
+  }
+  return count
+  
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2])) //2
+show(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) //7
+console.log(countUniqueValues([])) // 0
+console.log(countUniqueValues([-2,-1,-1,0,1])) // 4
 
